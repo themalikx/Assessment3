ce93c5f30fe2c158254f622339140145f7dab2d2
themalikx <iffi.malikx@hotmail.com>
2023-02-21 18:01:44 +0400

    seeding data

---
+++ Assesment3/Db/Migrations/20230221095342_SeedData.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Assesment3.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Author",
+                columns: new[] { "Id", "DateOfBirth", "Name" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1989, 4, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Bruce Herbert" },
+                    { 2, new DateTime(1989, 4, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "By Svetlin Nakov" },
+                    { 3, new DateTime(1969, 4, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Josef Miller-Brockmann" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "BookCategory",
+                columns: new[] { "Id", "Name", "ParentCategoryId" },
+                values: new object[,]
+                {
+                    { 1, "Literature", 0 },
+                    { 2, "Poetry", 1 },
+                    { 3, "Humour and satire", 1 },
+                    { 4, "Fantasy", 1 },
+                    { 5, "Information Tech", 0 },
+                    { 6, "Computer Programming", 5 },
+                    { 7, "Graphics and Designs", 5 },
+                    { 8, "Machine Learning", 5 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Book",
+                columns: new[] { "Id", "AuthorId", "CategoryId", "Subtitle", "Title", "YearOfRelease" },
+                values: new object[,]
+                {
+                    { 1, 1, 6, "", "C# (C Sharp Programming) : a Step by Step Guide for Beginners", new DateTime(2004, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, 3, 8, "", "Dive into Neural Networks Using C Sharp", new DateTime(2016, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3, 2, 7, "The Bulgarian C# Book", "Grid Systems in Graphic Design", new DateTime(2016, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Book",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Book",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Book",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "BookCategory",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "BookCategory",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "BookCategory",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "BookCategory",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "BookCategory",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Author",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Author",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Author",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "BookCategory",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "BookCategory",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "BookCategory",
+                keyColumn: "Id",
+                keyValue: 8);
+        }
+    }
+}
--- Assesment3/Db/Seeder.cs
+++ Assesment3/Db/Seeder.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Assesment3.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Assesment3.Db;
 
@@ -6,6 +7,100 @@ public class Seeder
 {
     public static void SeedDataIntoDatabase(ModelBuilder builder)
     {
-            
+        builder.Entity<Author>().HasData(
+            new Author
+            {
+                Id = 1,
+                Name = "Bruce Herbert",
+                DateOfBirth = new DateTime(1989, 4, 1)
+            }, new Author
+            {
+                Id = 2,
+                Name = "By Svetlin Nakov",
+                DateOfBirth = new DateTime(1989, 4, 1)
+            },
+            new Author
+            {
+                Id = 3,
+                Name = "Josef Miller-Brockmann",
+                DateOfBirth = new DateTime(1969, 4, 1)
+            });
+
+        builder.Entity<BookCategory>().HasData(
+            new BookCategory()
+            {
+                Id = 1,
+                Name = "Literature",
+            },
+            new BookCategory()
+            {
+                Id = 2,
+                Name = "Poetry",
+                ParentCategoryId = 1
+            },
+            new BookCategory()
+            {
+                Id = 3,
+                Name = "Humour and satire",
+                ParentCategoryId = 1
+            }, new BookCategory()
+            {
+                Id = 4,
+                Name = "Fantasy",
+                ParentCategoryId = 1
+            },
+            new BookCategory()
+            {
+                Id = 5,
+                Name = "Information Tech",
+            },
+            new BookCategory()
+            {
+                Id = 6,
+                Name = "Computer Programming",
+                ParentCategoryId = 5
+            },
+            new BookCategory()
+            {
+                Id = 7,
+                Name = "Graphics and Designs",
+                ParentCategoryId = 5
+            },
+            new BookCategory()
+            {
+                Id = 8,
+                Name = "Machine Learning",
+                ParentCategoryId = 5
+            }
+            );
+
+
+        builder.Entity<Book>().HasData(
+            new Book
+            {
+                Id = 1,
+                Title = "C# (C Sharp Programming) : a Step by Step Guide for Beginners",
+                Subtitle = "",
+                YearOfRelease = new DateTime(2004, 1, 1),
+                AuthorId = 1, CategoryId = 6
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "Dive into Neural Networks Using C Sharp",
+                Subtitle = "",
+                YearOfRelease = new DateTime(2016, 1, 1),
+                AuthorId = 3,
+                CategoryId = 8
+
+            }, new Book
+            {
+                Id = 3,
+                Title = "Grid Systems in Graphic Design",
+                Subtitle = "The Bulgarian C# Book",
+                YearOfRelease = new DateTime(2016, 1, 1),
+                AuthorId = 2,
+                CategoryId = 7
+            });
     }
 }
